8d537ea60a864402aa36e1e4bcb74e611816db2f
Kamil Ogonowski <kml.ogonowski@gmail.com>
2023-08-05 00:45:12 +0200

    creating of inital variables in procedural code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        float montlhyPayment;
+        float principal;
+        float annualInterestRate;
+        float monthlyInterestRate;
+        int numberOfPayments;
+
+
+        System.out.println("Principal: ");
+        principal = scanner.nextFloat();
+
+        System.out.println("Annual InterestRate: ");
+        annualInterestRate = scanner.nextFloat();
+
+        System.out.println("Period (Years): ");
+        numberOfPayments = scanner.nextInt();
+
+
+        System.out.println(principal + " prinicpal/ " + annualInterestRate + " annualInterestRate/ " + numberOfPayments + " numberOfPayments/ ");
     }
 }
