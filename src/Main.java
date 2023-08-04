ca68d696cfd97e2bd6044239c35da736870b9cb9
Kamil Ogonowski <kml.ogonowski@gmail.com>
2023-08-05 01:27:25 +0200

    procedural code calculating of mortgage, prone to invalid user's input

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,38 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
 
-        Scanner scanner = new Scanner(System.in);
-        float montlhyPayment;
         float principal;
         float annualInterestRate;
         float monthlyInterestRate;
-        int numberOfPayments;
+        byte years;
 
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Principal: ");
         principal = scanner.nextFloat();
 
         System.out.println("Annual InterestRate: ");
         annualInterestRate = scanner.nextFloat();
+        monthlyInterestRate = annualInterestRate/MONTHS_IN_YEAR/PERCENT;
 
         System.out.println("Period (Years): ");
-        numberOfPayments = scanner.nextInt();
+        years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
 
+        double mortgage = principal
+                *(monthlyInterestRate*(Math.pow(1 + monthlyInterestRate, numberOfPayments)))
+                /(Math.pow(1 + monthlyInterestRate, numberOfPayments)-1);
 
-        System.out.println(principal + " prinicpal/ " + annualInterestRate + " annualInterestRate/ " + numberOfPayments + " numberOfPayments/ ");
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        String monthlyPaymentConverted = formatter.format(mortgage);
+//        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+
+        System.out.println("Monthly payment is equal to: " + monthlyPaymentConverted);
+
     }
 }
