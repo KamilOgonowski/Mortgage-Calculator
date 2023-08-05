7992acfb226004912c8a46c00730aa0fe8069d9d
Kamil Ogonowski <kml.ogonowski@gmail.com>
2023-08-05 14:54:32 +0200

    Creating of a method to get mortgage parameters form user

--- src/Main.java
+++ src/Main.java
@@ -11,34 +11,10 @@ public class Main {
         float monthlyInterestRate;
         byte years;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Principal ($1K - $1M): ");
-        while (true){
-            principal = scanner.nextFloat();
-            if (principal > 1_000 && principal < 1_000_000){
-                break;
-            } else
-                System.out.println("Enter a number between 1,000 and 1,000,000.");
-        }
-
-        System.out.println("Annual InterestRate: ");
-        while (true){
-            annualInterestRate = scanner.nextFloat();
-            if (annualInterestRate > 0 && annualInterestRate <= 30){
-                break;
-            } else
-                System.out.println("Enter a value greater than 0 and less than or equal to 30.");
-        }
+        principal = (float) readNumber("Principal",1000, 1_000_000);
+        annualInterestRate = (float) readNumber("Annual Interest Rate",0, 30);
+        years = (byte) readNumber("Years (Period)",0, 30);
         monthlyInterestRate = annualInterestRate/MONTHS_IN_YEAR/PERCENT;
-
-        System.out.println("Period (Years): ");
-        while (true){
-            years = scanner.nextByte();
-            if (years > 0 && years <= 30){
-                break;
-            } else
-                System.out.println("Enter a value between 1 and 30.");
-        }
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal
@@ -53,5 +29,17 @@ public class Main {
 
     }
 
+
+    public static double readNumber(String prompt, int min, int max){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println(prompt + ": ");
+            double value = scanner.nextDouble();
+            if (value > min && value < max){
+                return value;
+            } else
+                System.out.println("Enter a number between "+ min + " and " + max);
+        }
+    }
 
 }
